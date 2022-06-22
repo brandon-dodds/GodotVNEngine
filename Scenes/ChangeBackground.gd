9b5c38907eb867ad14fb0280a3abab38cf001f12
Brandon <brandon.lee.dodds@gmail.com>
2022-06-22 14:55:42 +0100

    Added scaling to imported background image.
    The ChangeBackground.gd file now contains code in the input function to change the scale of the sprite depending on what the original scale of the image is.

--- Scenes/ChangeBackground.gd
+++ Scenes/ChangeBackground.gd
@@ -1,6 +1,10 @@
 extends Sprite
-onready var backgroundtexture = load("res://TestAssets/Sol_Guilty_Gear_Strive.webp")
+onready var backgroundtexture = load("res://TestAssets/MeakashiCG_(6).webp")
 
 func _input(event):
 	if event.is_action_pressed("change_background"):
+		var viewportWidth = get_viewport().size.x
+		var viewportHeight = get_viewport().size.y
+		var width_scale = viewportWidth / backgroundtexture.get_size().x
+		set_scale(Vector2(width_scale, width_scale))
 		self.texture = backgroundtexture
