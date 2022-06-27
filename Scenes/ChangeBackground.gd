87b9888a9b774ec4e3413c3bfc66b9b89c68acef
Brandon <brandon.lee.dodds@gmail.com>
2022-06-27 21:44:12 +0100

    Changed background scaling and added a basic regex.
    Background scaling: ChangeBackground.gd
    Basic Regex: ScriptReader.gd

--- Scenes/ChangeBackground.gd
+++ Scenes/ChangeBackground.gd
@@ -6,5 +6,6 @@ func _input(event):
 		var viewportWidth = get_viewport().size.x
 		var viewportHeight = get_viewport().size.y
 		var width_scale = viewportWidth / backgroundtexture.get_size().x
-		set_scale(Vector2(width_scale, width_scale))
+		var height_scale = viewportHeight / backgroundtexture.get_size().y
+		set_scale(Vector2(width_scale, height_scale))
 		self.texture = backgroundtexture
---
+++ Scenes/ScriptReader.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func loadFile():
+	var file = File.new()
+	file.open("res://Scripts/MainScript.txt", File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return content
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var x = loadFile()
+	var regex = RegEx.new()
+	regex.compile("(?:background) (.+$)")
+	var result = regex.search(x)
+	if result:
+		print(result.get_string(1))
